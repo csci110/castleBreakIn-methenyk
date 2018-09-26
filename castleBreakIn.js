0ac7cc74ec87dd075ff84e301069a3e7f794ecc8
Katyanna Metheny <methenyk@dewv.edu>
2018-09-26 16:51:18 +0000

    Updates via sync script

--- castleBreakIn.js
+++ castleBreakIn.js
@@ -14,7 +14,7 @@ class wall extends Sprite {
 }
 new wall(0, 0, "A spooky castle wall", "castle.png");
 
-let leftWall = new wall(0, 200, "Letf side wall", "wall.png");
+let leftWall = new wall(0, 200, "Left side wall", "wall.png");
 
 let rightWall = new wall(game.displayWidth- 48, 200, "Right side wall", "wall.png");
 
@@ -32,6 +32,55 @@ class Princess extends Sprite {
         this.accelerateOnBounce = false;
         this.defineAnimation("left", 9, 11);
         this.defineAnimation("right", 3, 5);
+        this.speedWhenWalking = 150;
+    }
+    handleLeftArrowKey(){
+    this.playAnimation ("left");
+    this.angle = 180;
+    this.speed = this.speedWhenWalking
+    }
+    handleRightArrowKey(){
+        this.playAnimation ("right");
+        this.angle = 360;
+        this.speed = this.speedWhenWalking;
+    }
+    handleGameLoop() {
+        this.y = Math.max(5, this.y); 
+        this.y = Math.min(game.displayHeight - this.height, this.y);
+        this.x = Math.max(0, this.x);
+        this.x = Math.min(game.displayWidth - this.width, this.x);
+        this.speed = 0;
+    }
+    handleCollision(otherSprite) {
+        let horizontalOffset = this.x - otherSprite.x;
+        let verticalOffset = this.y - otherSprite.y;
+        if (Math.abs(horizontalOffset) < this.width / 3 
+                    && verticalOffset > this.height / 4) {
+            otherSprite.angle = 90 + 2 * horizontalOffset;
+        }
+        return false;
     }
 }
 let Ann = new Princess();
+
+class Ball extends Sprite {
+    constructor(x, y, name, image){
+        super();
+        this.height = 48;
+        this.width = 48;
+        this.x = x;
+        this.y = y;
+        this.name = "name";
+        this.setImage(image);
+        this.defineAnimation("spin", 0, 11);
+        this.playAnimation("spin", true);
+        this.speed = 1;
+        this.angle = 50 + Math.random() * 80;
+    }
+    handleGameLoop(){
+        if(this.speed < 200){
+            this.speed ++  ;
+        }
+    }
+}
+new Ball(350, 350, "A ball", "ball.png");
